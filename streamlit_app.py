5fae8c6cb1b91a8560f8ec868fcbaa9660969b1c
Guido-wu <guido.wurges@epfl.ch>
2026-05-06 14:20:18 +0200

    Image - Background

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import base64
 
 
 
@@ -28,4 +29,40 @@ st.markdown(
     """, 
     unsafe_allow_html=True
 )
+
+
+
+
+
+
+#Ajout d'une image de background dans l'application
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+img_base64 = get_base64_of_bin_file('Biblio.jpeg')
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: linear-gradient(rgba(255, 255, 255, 0.7), rgba(255, 255, 255, 0.7)), 
+        url("data:image/jpg;base64,{img_base64}");
+        background-attachment: fixed;
+        background-size: cover;
+        backdrop-filter: blur(5px); /* Ajoute un léger flou */
+    }
+
+    /* Optionnel : Assure-toi que ton texte est bien noir/sombre pour le contraste */
+    h1, p {
+        color: #1A1A1A !important;
+        text-shadow: 1px 1px 2px rgba(255,255,255,0.5);
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 
