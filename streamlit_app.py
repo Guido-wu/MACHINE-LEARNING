0a8fb2bdec6d570c96bf4b36384d79a792796941
Guido-wu <guido.wurges@epfl.ch>
2026-05-06 13:45:18 +0200

    ajout titre de fond

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 
-st.title("Mon Recommandeur de Livres")
+st.title("Welcome to our books recomendation app !!")
+st.subheader("Find the book that fits you the best !")
 
 st.write("Hello ! Si tu vois ce message, c'est que l'interface fonctionne.")
 st.balloons() 
