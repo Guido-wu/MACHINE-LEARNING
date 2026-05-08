2b6a2ad6749a57f24d4b574962f01d2e4552d5a1
Guido-wu <guido.wurges@epfl.ch>
2026-05-08 09:18:02 +0200

    User_id_test

--- streamlit_app.py
+++ streamlit_app.py
@@ -151,12 +151,13 @@ df_items, df_reco = load_data()
 user_id = st.number_input("Entrez votre User ID :", min_value=0, max_value=len(df_reco)-1, step=1)
 
 
-if st.button("Voir mes livres"):
+if st.button("My books recommandations"):
     # On appelle la fonction qui vient de l'autre fichier
-    mes_livres = afficher_recommandations(user_id, df_reco, df_items)
-    
-    if mes_livres is not None:
-        for index, row in mes_livres.iterrows():
+    Books = afficher_recommandations(user_id, df_reco, df_items)
+'''
+    if Books is not None:
+        for index, row in Books.iterrows():
             st.write(f"📖 **{row['title']}**")
     else:
         st.error("Utilisateur inconnu")
+'''
