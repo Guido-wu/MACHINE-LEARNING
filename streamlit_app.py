b43f8e262f7ef726cc7996447c47ee2c4a860cd0
Guido-wu <guido.wurges@epfl.ch>
2026-05-09 15:16:08 +0200

    bg

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 from item_to_reco import afficher_recommandations
 from Bandeau import afficher_bandeau_covers
-
+form Photo_Fond import afficher_image_fond
 
 
 # 1. CONFIGURATION DE LA PAGE
@@ -184,24 +184,8 @@ with st.sidebar:
 
 
 # 5. CONTENU PRINCIPAL - Chaque page de notre site 
-if menu == "Home":
-    import base64
-    def get_base64(bin_file):
-        with open(bin_file, 'rb') as f:
-             data = f.read()
-        return base64.b64encode(data).decode()
 
-#Backgroud logo
-    try:
-        bin_str = get_base64("Logo_2.png") 
-        st.markdown(
-            f'<img src="data:image/png;base64,{bin_str}" class="background-logo">',            
-            unsafe_allow_html=True
-        )
-    except:
-        pass
-
-
+    afficher_image_fond("Logo_2.png")
     st.markdown("<h1 style='color: #1a1a1a;'>Welcome to The Bookworm</h1>", unsafe_allow_html=True)
     
     st.divider()
