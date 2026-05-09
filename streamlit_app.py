e3f2ae54ea6bf523e3461406b5771aae2d712005
Guido-wu <guido.wurges@epfl.ch>
2026-05-09 12:38:46 +0200

    sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,7 +89,7 @@ df_items, df_reco, df_enriched = load_data()
 
 # 4. SIDEBAR (MENU DE GAUCHE)
 with st.sidebar:
-    st.h2("The Bookworm")
+    st.markdown("The Bookworm")
     st.image("a9e8a2a7-e0a4-422d-b5a9-57ae442fb57a.png")
     
     st.markdown("---")
